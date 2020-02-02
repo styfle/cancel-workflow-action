2ea9b4383271bff0f507b3cab816fba9f9040620
Steven <steven@ceriously.com>
2020-02-01 19:53:40 -0500

    Print contenxt

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ try {
   const workflow = core.getInput('workflow-name');
   console.log(`Found input ${workflow}!`);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(Object.keys(github.context), undefined, 2)
+  delete github.context.payload;
+  const payload = JSON.stringify(github.context, undefined, 2)
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
