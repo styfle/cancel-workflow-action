81d0ede75b6752c59c23ec3d158d38f68729dcf1
Steven <steven@ceriously.com>
2020-02-01 19:42:40 -0500

    Rebuild

--- index.js
+++ index.js
@@ -8267,11 +8267,10 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  console.log('Secrets ' + typeof github.secrets)
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
