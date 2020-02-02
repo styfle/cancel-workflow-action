72f3bc6b3d01e2cc474ab8915314b501d88daeea
Steven <steven@ceriously.com>
2020-02-01 20:24:11 -0500

    Add cancel logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const statusSet = new Set(['queued', 'in_progress']);
 
-try {
-  const { eventName, sha, ref, repo } = github.context;
-  const workflow = core.getInput('workflow-name');
-  const token = core.getInput('access-token');
+async function main() {
+  const { eventName, sha, ref, repo: { owner, repo } } = github.context;
+  const branch = ref.slice(11);
+  console.log({ eventName, sha, branch, owner, repo });
+  const workflow_id = core.getInput('workflow-name', { required: true });
+  const token = core.getInput('access-token', { required: true });
   console.log(`Found input: ${workflow}`);
   console.log(`Found token: ${token ? 'yes' : 'no'}`);
-  console.log({ eventName, sha, ref, repo });
-} catch (error) {
-  core.setFailed(error.message);
+  const octokit = new github.GitHub(token);
+  const { data } = await octokit.actions.listWorkflowRuns({ workflow_id });
+  console.log(`Found ${data.total_count} runs total.`);
+  const others = data.workflow_runs.filter(
+    o => o.head_branch === branch && o.head_sha !== sha && statusSet.has(o.status)
+  );
+  console.log(`Found ${others.length} runs in progress.`);
+  for (const { id, head_sha, status } of others) {
+    console.log('Cancelling another run: ', { id, head_sha, status });
+    const res = await octokit.actions.cancelWorkflowRun({ run_id: id });
+    console.log(`Status ${res.status}`);
+  }
+  console.log('Done.');
 }
+
+main()
+  .then(() => core.info('Cancel Complete.'))
+  .catch(e => core.setFailed(e.message));
