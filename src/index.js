5e8b12b5b2fe1eb0c12b8afcfb9d958969eee426
ahanriat <ahanriat@gmail.com>
2020-02-11 12:43:22 +0000

    Fix branch name when the even is a pull request

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function main() {
-  const { eventName, sha, ref, repo: { owner, repo } } = github.context;
-  const branch = ref.slice(11);
-  console.log({ eventName, sha, branch, owner, repo });
+  const { eventName, sha, ref, repo: { owner, repo }, payload } = github.context;
+  let branch = ref.slice(11);
+  let headSha = sha;
+  if (payload.pull_request) {
+    branch = payload.pull_request.head.ref;
+    headSha = payload.pull_request.head.sha;
+  }
+
+  console.log({ eventName, sha, headSha, branch, owner, repo });
   const workflow_id = core.getInput('workflow_id', { required: true });
   const token = core.getInput('access_token', { required: true });
   console.log(`Found input: ${workflow_id}`);
   console.log(`Found token: ${token ? 'yes' : 'no'}`);
   const octokit = new github.GitHub(token);
-  const { data } = await octokit.actions.listWorkflowRuns({ owner, repo, workflow_id });
+  const { data } = await octokit.actions.listWorkflowRuns({ owner, repo, workflow_id, branch });
   console.log(`Found ${data.total_count} runs total.`);
-  const others = data.workflow_runs.filter(
-    o => o.head_branch === branch && o.head_sha !== sha && o.status !== 'completed'
+  const runningWorkflows = data.workflow_runs.filter(
+    workflow => workflow.head_sha !== headSha && workflow.status !== 'completed'
   );
-  console.log(`Found ${others.length} runs in progress.`);
-  for (const { id, head_sha, status } of others) {
+  console.log(`Found ${runningWorkflows.length} runs in progress.`);
+  for (const { id, head_sha, status } of runningWorkflows) {
     console.log('Cancelling another run: ', { id, head_sha, status });
     const res = await octokit.actions.cancelWorkflowRun({ owner, repo, run_id: id });
     console.log(`Status ${res.status}`);
