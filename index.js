fa14f139ff6fdbe26660bc9599e3f1ebc01db8f0
Markus Podar <markus@fischer.name>
2020-03-22 19:22:53 +0100

    Support comma separated IDs for workflow_id (#4)
    Allows to specify multiple workflow IDs at once without
    having to invoke the tool multiple times.

--- index.js
+++ index.js
@@ -8274,27 +8274,47 @@ async function main() {
 
   console.log({ eventName, sha, headSha, branch, owner, repo });
   const workflow_id = core.getInput('workflow_id', { required: true });
+  const workflow_ids = workflow_id.replace(/\s/g, '').split(',');
   const token = core.getInput('access_token', { required: true });
   console.log(`Found input: ${workflow_id}`);
   console.log(`Found token: ${token ? 'yes' : 'no'}`);
   const octokit = new github.GitHub(token);
-  const { data } = await octokit.actions.listWorkflowRuns({ owner, repo, workflow_id, branch });
-  console.log(`Found ${data.total_count} runs total.`);
-  const runningWorkflows = data.workflow_runs.filter(
-    workflow => workflow.head_sha !== headSha && workflow.status !== 'completed'
-  );
-  console.log(`Found ${runningWorkflows.length} runs in progress.`);
-  for (const { id, head_sha, status } of runningWorkflows) {
-    console.log('Cancelling another run: ', { id, head_sha, status });
-    const res = await octokit.actions.cancelWorkflowRun({ owner, repo, run_id: id });
-    console.log(`Status ${res.status}`);
-  }
+
+  await Promise.all(workflow_ids.map(async (workflow_id) => {
+    try {
+      const {data} = await octokit.actions.listWorkflowRuns({
+        owner,
+        repo,
+        workflow_id,
+        branch
+      });
+      console.log(`Found ${data.total_count} runs total.`);
+      const runningWorkflows = data.workflow_runs.filter(
+        workflow => workflow.head_sha !== headSha && workflow.status !== 'completed'
+      );
+      console.log(`Found ${runningWorkflows.length} runs in progress.`);
+      for (const {id, head_sha, status} of runningWorkflows) {
+        console.log('Cancelling another run: ', {id, head_sha, status});
+        const res = await octokit.actions.cancelWorkflowRun({
+          owner,
+          repo,
+          run_id: id
+        });
+        console.log(`Status ${res.status}`);
+      }
+    } catch (e) {
+      const msg = e.message || e;
+      console.log(`Error while cancelling workflow_id ${workflow_id}: ${msg}`);
+    }
+  }));
+
   console.log('Done.');
 }
 
 main()
   .then(() => core.info('Cancel Complete.'))
   .catch(e => core.setFailed(e.message));
+
 
 /***/ }),
 
