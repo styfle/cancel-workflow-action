a91628b4adc5e2009e1fffa20c35e7777905e901
Steven <steven@ceriously.com>
2020-06-07 20:04:09 -0400

    Bump deps

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,8 @@ if (!core) {
 }
 
 async function main() {
-  console.log({ action: github.context.action });
-  console.log({ actor: github.context.actor });
-  console.log({ payload: github.context.payload });
   const { eventName, sha, ref, repo: { owner, repo }, payload } = github.context;
+  const { GITHUB_RUN_ID } = process.env;
   let branch = ref.slice(11);
   let headSha = sha;
   if (payload.pull_request) {
@@ -21,16 +19,32 @@ async function main() {
     headSha = payload.pull_request.head.sha;
   }
 
-  console.log({ eventName, sha, headSha, branch, owner, repo });
-  const workflow_id = core.getInput('workflow_id', { required: false });
-  /*if (!workflow_id) {
-    process.env.GITHUB_RUN_ID
-  }*/
-  const workflow_ids = workflow_id.replace(/\s/g, '').split(',').map(s => Number(s));
+  console.log({ eventName, sha, headSha, branch, owner, repo, GITHUB_RUN_ID });
   const token = core.getInput('access_token', { required: true });
-  console.log(`Found workflow_id: ${workflow_id}`);
+  const workflow_id = core.getInput('workflow_id', { required: false });
   console.log(`Found token: ${token ? 'yes' : 'no'}`);
-  const octokit = new github.GitHub(token);
+  const workflow_ids: number[] = [];
+  const octokit = github.getOctokit(token);
+
+  if (workflow_id) {
+    // The user provided one or more workflow id
+    workflow_id.replace(/\s/g, '')
+      .split(',')
+      .map(s => Number(s))
+      .forEach(n => workflow_ids.push(n));
+  } else if (GITHUB_RUN_ID) {
+    // The user did not provide workflow id so derive from current run
+    const { data } = await octokit.actions.getWorkflowRun({
+      owner,
+      repo,
+      run_id: Number(GITHUB_RUN_ID)
+    });
+    workflow_ids.push(data.workflow_id);
+  } else {
+    throw new Error('Expected `workflow_id` input or `GITHUB_RUN_ID` env var');
+  }
+
+  console.log(`Found workflow_id: ${JSON.stringify(workflow_ids)}`);
 
   await Promise.all(workflow_ids.map(async (workflow_id) => {
     try {
